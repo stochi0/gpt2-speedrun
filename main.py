f8a8120966d595bf1ee73a30d536c99fdccdf00c
stochi0 <233351902+stochi0@users.noreply.github.com>
2026-01-02 02:28:21 +0530

    feat: implement GPT2 model structure with config and base classes

---
+++ main.py
@@ -0,0 +1,52 @@
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+
+torch.manual_seed(1337)
+
+# gpt2 config 
+
+@dataclass
+class GPT2Config:
+    sequence_window_size: int = 1024
+    vocab_size: int = 50257
+    n_layer: int = 12
+    n_head: int = 12
+    n_embd: int = 768
+
+
+# gpt2 model
+
+class GPT2Attention(nn.Module):
+    pass
+
+class GPT2MLP(nn.Module):
+    pass
+
+class GPT2Block(nn.Module):
+    def __init__(self, config: GPT2Config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = GPT2Attention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = GPT2MLP(config)
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        pass
+
+class GPT2Model(nn.Module):
+    def __init__(self, config: GPT2Config):
+        super().__init__()
+        self.config = config
+        self.transformer = nn.ModuleDict(
+            {
+                "wte": nn.Embedding(config.vocab_size, config.n_embd),
+                "wpe": nn.Embedding(config.sequence_window_size, config.n_embd),
+                "h": nn.ModuleList([GPT2Block(config) for _ in range(config.n_layer)]),
+                "ln_f": nn.LayerNorm(config.n_embd),
+            }
+        )
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x
