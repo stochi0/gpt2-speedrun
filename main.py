092555e797856b736cdde4174de2776e4d417b68
stochi0 <233351902+stochi0@users.noreply.github.com>
2026-01-02 14:42:02 +0530

    feat: GPT2Attention with forward method and attention score calculation

--- main.py
+++ main.py
@@ -20,10 +20,25 @@ class GPT2Config:
 class GPT2Attention(nn.Module):
     def __init__(self, config: GPT2Config):
         super().__init__()
-        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd) # [Q|K|V] -- fused QKV for speed up
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
         self.n_head = config.n_head
         self.n_embd = config.n_embd
+        self.head_dim = config.n_embd // config.n_head
+        self.bias = torch.tril(torch.ones(config.sequence_window_size, config.sequence_window_size))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+            B, T, D = x.shape
+            Q, K, V = self.c_attn(x).split(self.n_embd, dim=2)
+            Q = Q.view(B, T, self.n_head, self.head_dim).transpose(1, 2)
+            K = K.view(B, T, self.n_head, self.head_dim).transpose(1, 2)
+            V = V.view(B, T, self.n_head, self.head_dim).transpose(1, 2)
+            attn_scores = Q @ K.transpose(-2, -1) * (self.head_dim ** -0.5)
+            attn_scores = attn_scores.mask_fill(self.bias[:, :T, :T] == 0, float("-inf"))
+            attn_scores = attn_scores.softmax(dim=-1)
+            attn = attn_scores @ V
+            attn = attn.transpose(1, 2).contiguous().view(B, T, D)
+            return self.c_proj(attn)
    
 class GPT2MLP(nn.Module):
     def __init__(self, config: GPT2Config):
